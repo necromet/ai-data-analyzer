247630ba35e1d987f2f8961ec18793ab515eef4b
Neukrom <edwardrenaldi219@gmail.com>
2026-01-22 16:43:18 +0700

    Checkpoint Graph

--- langgraph_app/src/agent/graph.py
+++ langgraph_app/src/agent/graph.py
@@ -1,16 +1,49 @@
+from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langchain.agents import create_agent
-from agent.system_prompt import SYSTEM_PROMPT
+from agent.text_to_sql_system_prompt import TEXT_TO_SQL_SYSTEM_PROMPT
+from agent.general_agent_system_prompt import GENERAL_AGENT_SYSTEM_PROMPT
+from openai import OpenAI
+
+class State(TypedDict):
+    joke: str
+    topic: str
+    feedback: str
+    funny_or_not: str
+
+# Configurations
+llm = OpenAI()
+
+
+def llm_call(state: State):
+    if state.get("feedback"):
+        msg = llm.responses.create(
+            model="gpt-4o-mini-2024-07-18"
+            input=f"Provide feedback on the joke: {state['joke']} about {state['topic']}. User feedback: {state['feedback']}"
+        )
+    else:
+        msg = llm.responses.create(
+            model="gpt-4o-mini-2024-07-18"
+            input=f"Provide feedback on the joke: {state['joke']} about {state['topic']}. User feedback: {state['feedback']}"
+        )
 
 agent = create_agent(
     "gpt-4o-mini-2024-07-18",
-    # tools=[send_email],
-    system_prompt=SYSTEM_PROMPT,
+    system_prompt=GENERAL_AGENT_SYSTEM_PROMPT,
 )
 
-graph = StateGraph(MessagesState)
-graph.add_node("agent", agent)
 
+graph = StateGraph(MessagesState)
+
+# Nodes
+graph.add_node("agent", agent)
+graph.add_node("text_to_sql", text_to_sql_calls)
+
+# Edges to connect nodes
 graph.add_edge(START, "agent")
+# graph.add_edge("agent", "text_to_sql")
 graph.add_edge("agent", END)
+# graph.add_edge("text_to_sql", "agent")
+
+# Compiler
 graph = graph.compile()
